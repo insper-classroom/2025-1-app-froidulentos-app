6c39fb02c0b1d725667958a2fb440092aef3a866
luigilopesz <luigilopes09@gmail.com>
2025-05-29 11:35:57 -0300

    atualizando rota que roda modelo, e adicionando funcao para listar modelos disponiveis.
    retorno de metricas como "lucro", "f1_score" etc nao estao implementadas ainda.
    
    Co-authored-by: Marcelo Alonso <125833252+marcelo-vta@users.noreply.github.com>
    
    Co-authored-by: Henrique Bucci Rodrigues Netto <97095738+henriquebrnetto@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -7,28 +7,55 @@ import joblib
 BASE_URL = '/api/v1'
 
 app = Flask("Fraudulentos API")
+
 CORS(app)
+
+
+@app.route(BASE_URL + '/get_models', methods=['GET'])
+def get_models():
+    
+    model_files = [model for model in os.listdir('../models') if model.endswith('.pkl')]
+    model_names = [os.path.splitext(model)[0] for model in model_files]
+    
+    return {'models': model_names}, 200
+
 
 @app.route(BASE_URL + '/add_data', methods=['POST'])
 def add_data():
 
-    data = request.json
-    if not data:
-        return {'error': 'No data provided'}, 400
-
-    # Save the new data to a file or database
-    # For simplicity, we will just print it here
-    print("New data received:", data)
-    
-    # Here you would typically append this data to your training dataset
-    return {'message': 'Data added successfully'}, 200
-
-
-
-@app.route(BASE_URL + '/predict', methods=['POST'])
-def prediction():
     pass
+
+
+@app.route(BASE_URL + '/run_model', methods=['POST'])
+def run_model():
     
+    try:
+        data = request.get_json()
+        model_name = data.get('model_name')
+
+        if not model_name:
+            return {'error': 'Model name is required'}, 400
+
+        model_path = f'../models/{model_name}.pkl'
+        
+        if not os.path.exists(model_path):
+            return {'error': 'Model not found'}, 404
+
+        model = joblib.load(model_path)
+        y = model.predict("dataset")
+        
+        return {
+                'model_name': model_name,
+                'profit': 1,  
+                'accuracy': 0.1,  
+                'precision': 0.1,
+                'recall': 0.1,
+                'f1_score': 0.1,
+                'y': y.tolist()
+        }, 200
+
+    except Exception as e:
+        return {'error': str(e)}, 500
     
 
 if __name__ == '__main__':
